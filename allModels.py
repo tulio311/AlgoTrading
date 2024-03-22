7d51c90eaeca542eda0e581a9c35d3250075d3ef
iMac <imac@iMac-de-iMac.local>
2024-03-21 20:09:34 -0600

    k-fold cv

--- allModels.py
+++ allModels.py
@@ -32,7 +32,9 @@ X = pd.DataFrame(x1,columns=['btc','eth','bch','xrp','labels'])
 fullData = X.iloc[:,:4]
 fullLabels = X.iloc[:,4]
 
-kFolds = KFold(n_splits=4,  random_state=100, shuffle=True)
+folds = 4
+
+kFolds = KFold(n_splits=folds,  random_state=100, shuffle=True)
 
 trainX,testX, trainY,testY = train_test_split(fullData,fullLabels,test_size=.3)
 
@@ -46,25 +48,36 @@ validLabels = X.iloc[:,4]
 n = len(validLabels)
 
 max = 0
+avg = 0
 
 # Naive Bayes Gaussiano
 
 for i, (train_index, test_index) in enumerate(kFolds.split(fullData)):
 
+    print((train_index, test_index))
+
     modelGNB = GaussianNB()
 
-    modelGNB.fit(data,Labels)
+    modelGNB.fit(fullData.iloc[train_index],fullLabels[train_index])
 
-    Y = modelGNB.predict(valid)
+    Y = modelGNB.predict(fullData.iloc[test_index]) 
 
     suma = 0
 
-    for i in range(n):
-        if Y[i] == validLabels[i]:
-            suma = suma + 1/n
+    print(Y)
+    valid = fullLabels.iloc[test_index].reset_index().iloc[:,1]
+    print(valid)
+
+    for j in range(len(Y)):
+        #print(fullLabels.iloc[test_index].iloc[j])
+        if Y[j] == valid.iloc[j]:
+            suma = suma + 1/len(Y)
+
+    avg = avg + suma/folds
+    print(suma)
     
-max = suma
-print("Naive Bayes, Correcto: ",max)
+max = avg
+print("Naive Bayes, Accuracy: ",max)
 
 
 
@@ -82,28 +95,45 @@ profs = [6,7,8,9,10]
 gammas = [0,0.1,0.2]
 
 ind = 0
+
 for arb in arboles:
     for eta in etas:
         for prof in profs:
             for gamma in gammas:
+                
+                avg = 0
                 print("arboles: ",arb,"eta: ",eta,"profundidad: ",prof,"gamma: ",gamma)
-                model = XGBClassifier(n_estimators=arb,objective='binary:logistic',eta=eta,
-                      max_depth=prof,gamma = gamma,min_child_weight = 1,scale_pos_weight = 1)
-                model.fit(data,Labels)
-            
-                Y = model.predict(valid)
-                suma = 0
-                for i in range(n):
-                    if Y[i] == validLabels[i]:
-                        suma = suma + 1/n
-                if suma > max:
+
+                for i, (train_index, test_index) in enumerate(kFolds.split(fullData)):
+
+                    model = XGBClassifier(n_estimators=arb,objective='binary:logistic',eta=eta,
+                        max_depth=prof,gamma = gamma,min_child_weight = 1,scale_pos_weight = 1)
+                    model.fit(fullData.iloc[train_index],fullLabels[train_index])
+                
+                    Y = model.predict(fullData.iloc[test_index])
+                    suma = 0
+                    valid = fullLabels.iloc[test_index].reset_index().iloc[:,1]
+
+                    for j in range(len(Y)):
+                        if Y[j] == valid.iloc[j]:
+                            suma = suma + 1/len(Y)
+
+                    print(suma)
+                    avg = avg + suma/folds
+
+
+                if avg > max:
                     ind = 1
                     arbOpt = arb
                     etaOpt = eta
                     profOpt = prof
                     gammaOpt = gamma
-                    max = suma
-                print("Correcto: ",suma)
+                    max = avg
+                print("Accuracy ",avg)
+
+
+
+
 
 # Random Forest
 
@@ -115,52 +145,68 @@ critOpt = ""
 
 for arb in arbolesF:
     for crit in crits:
+        avg = 0
         print("arbolesF: ",arb,"crit: ",crit)
-        model = RandomForestClassifier(n_estimators=arb,criterion=crit)
-        model.fit(data,Labels)
-            
-        Y = model.predict(valid)
-        suma = 0
-        for i in range(n):
-            if Y[i] == validLabels[i]:
-                suma = suma + 1/n
-        if suma > max:
+
+        for i, (train_index, test_index) in enumerate(kFolds.split(fullData)):
+            model = RandomForestClassifier(n_estimators=arb,criterion=crit)
+            model.fit(fullData.iloc[train_index],fullLabels[train_index])
+                
+            Y = model.predict(fullData.iloc[test_index])
+            suma = 0
+            valid = fullLabels.iloc[test_index].reset_index().iloc[:,1]
+
+            for j in range(len(Y)):
+                if Y[j] == valid.iloc[j]:
+                    suma = suma + 1/len(Y)
+            print(suma)
+            avg = avg + suma/folds
+
+        if avg > max:
             ind = 2 
-            max = suma
+            max = avg
             arbFOpt = arb
             critOpt = crit
-        print("Correcto: ",suma)
+        print("Accuracy: ",avg)
 
 
 # SVC
         
 
-Cs = [0.8,1,1.2]
+Cs = [0.8,1.2]
 kernels = ["linear", "poly", "rbf", "sigmoid"]
 
 
 COpt = 0
 kernelOpt = 0
-gammaSVCOpt = 0
 
 for C in Cs:
     for kernel in kernels:
+
+        avg = 0
         print("C: ",C,"kernel: ",kernel)
-        model = SVC(C= C,kernel= kernel)
-        model.fit(data,Labels)
-            
-        Y = model.predict(valid)
-        suma = 0
-        for i in range(n):
-            if Y[i] == validLabels[i]:
-                suma = suma + 1/n
-        if suma > max:
+
+        for i, (train_index, test_index) in enumerate(kFolds.split(fullData)):
+            model = SVC(C= C,kernel= kernel)
+            model.fit(fullData.iloc[train_index],fullLabels[train_index])
+                
+            Y = model.predict(fullData.iloc[test_index])
+            suma = 0
+            valid = fullLabels.iloc[test_index].reset_index().iloc[:,1]
+
+            for j in range(len(Y)):
+                if Y[j] == valid.iloc[j]:
+                    suma = suma + 1/len(Y)
+            print(suma)
+            avg = avg + suma/folds
+
+        if avg > max:
             ind = 3 
-            max = suma
+            max = avg
             COpt = C
             kernelOpt = kernel
            
-        print("Correcto: ",suma)
+        print("Accuracy: ",avg)
       
 
 
